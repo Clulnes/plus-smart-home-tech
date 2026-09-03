206d933478f8982861dda80f8b4598c837679176
Clulnes <art.yusupov2003@yandex.ru>
2026-09-03 13:12:09 +0300

    Спринт 21(исправления6)

--- commerce/inventory-service/src/main/java/ru/yandex/practicum/commerce/inventory/InventoryServiceApplication.java
+++ commerce/inventory-service/src/main/java/ru/yandex/practicum/commerce/inventory/InventoryServiceApplication.java
@@ -2,16 +2,10 @@ package ru.yandex.practicum.commerce.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = "ru.yandex.practicum.commerce.inventory")
-@EnableJpaRepositories(basePackages = "ru.yandex.practicum.commerce.inventory.repository")
-@EntityScan(basePackages = "ru.yandex.practicum.commerce.inventory.model")
 public class InventoryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
--- commerce/product-service/src/main/java/ru/yandex/practicum/commerce/product/ProductServiceApplication.java
+++ commerce/product-service/src/main/java/ru/yandex/practicum/commerce/product/ProductServiceApplication.java
@@ -2,16 +2,10 @@ package ru.yandex.practicum.commerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = "ru.yandex.practicum.commerce.product")
-@EnableJpaRepositories(basePackages = "ru.yandex.practicum.commerce.product.repository")
-@EntityScan(basePackages = "ru.yandex.practicum.commerce.product.model")
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
