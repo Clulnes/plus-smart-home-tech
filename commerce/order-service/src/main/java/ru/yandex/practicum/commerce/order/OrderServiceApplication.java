f4da3aa47eb854cd14ba7a1ad811f3c1c9e41308
Clulnes <art.yusupov2003@yandex.ru>
2026-09-03 11:50:54 +0300

    Спринт 21(исправления5)

--- commerce/order-service/src/main/java/ru/yandex/practicum/commerce/order/OrderServiceApplication.java
+++ commerce/order-service/src/main/java/ru/yandex/practicum/commerce/order/OrderServiceApplication.java
@@ -2,10 +2,16 @@ package ru.yandex.practicum.commerce.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "ru.yandex.practicum.commerce.order")
+@EnableJpaRepositories(basePackages = "ru.yandex.practicum.commerce.order.repository")
+@EntityScan(basePackages = "ru.yandex.practicum.commerce.order.model")
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
--- commerce/product-service/src/main/java/ru/yandex/practicum/commerce/product/ProductServiceApplication.java
+++ commerce/product-service/src/main/java/ru/yandex/practicum/commerce/product/ProductServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "ru.yandex.practicum.commerce.product")
 @EnableJpaRepositories(basePackages = "ru.yandex.practicum.commerce.product.repository")
 @EntityScan(basePackages = "ru.yandex.practicum.commerce.product.model")
 public class ProductServiceApplication {
